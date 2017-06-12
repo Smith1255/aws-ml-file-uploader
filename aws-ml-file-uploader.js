34162857b5cda2a1d33ede5ea76142501974dfff
Andrew Smith <andy-smith@comcast.net>
2017-06-12 11:14:52 -0400

    Added Comments

--- aws-ml-file-uploader.js
+++ aws-ml-file-uploader.js
@@ -1,15 +1,30 @@
+/**
+ * This script takes a CSV file and its data schema and uploads it to the Amazon AWS S3 database.
+ * From there it creates an AWS Machine Learning datasource and model from the CSV data.
+ * In the future, ability to query the model for predicitons will be added.
+ *
+ * NOTE: To successfully use this script, you must include a valid CSV file, a valid schema
+ * (valid as in the same format that Amazon uses), and a policy.json that permits Amazon to
+ * read from the S3 buckets. These  files MUST be in a directory titled requiredFiles, also in the working
+ * directory of the program.
+ * The policy included in the requiredDocuments folder can be modified by changing the Bucket name and the CSV file name.
+ *
+ * @summary   Takes a CSV file and its data schema, uploads it to the Amazon AWS S3 database, and creates an AWS ML model.
+ * @author Andrew Smith
+ * 6-12-2017
+ *
+ */
 var AWS = require('aws-sdk');
-AWS.config.update({region:'us-east-1'});
 var machinelearning = new AWS.MachineLearning({apiVersion: '2014-12-12'});
-var createBucket = require('./createBucket.js');
 var s3 = new AWS.S3();
-var bucketName = process.argv[2];
-var uploadedFile = process.argv[3];
-var dataSchema = process.argv[4];
+AWS.config.update({region:'us-east-1'});
+
 var async = require('async');
 var glob = require('glob');
-var path = require('path');
 var globOptions = {dot:true};
+var path = require('path');
+
+var createBucket = require('./createBucket.js');
 // Bucket names must be unique across all S3 users
 var bucketName = "testAS1043";
 var dataParams;
@@ -17,6 +32,13 @@ var mlParams;
 var csv;
 var schema;
 
+/**
+ * @summary This function creates the parameters needed for each of the Amazon functions that communicate between AWS
+ * and the local machine.
+ *
+ *
+ * @return no return.
+ */
 
 function createParameters(done) {
     glob(path.join('requiredFiles/*.csv*'), globOptions, function(err, matches) {
@@ -44,20 +66,20 @@ async.series([
     function(callback) {
         createParameters(function() {
             console.log("Parameters for bucket created.")
-            callback();
+            callback(err);
         });
     },
     function(callback){
         createBucket(bucketName, function(err) {
             console.log("Created Bucket");
-            callback();
+            callback(err);
         });
     },
     function(callback){
         machinelearning.createDataSourceFromS3(dataParams, function(err, data) {
             if (err) console.log("ERR" + err, err.stack); // an error occurred
             console.log('Data Source Created for ML');
-            callback();
+            callback(err);
         });
     },
     function(callback){
--- createBucket.js
+++ createBucket.js
@@ -1,16 +1,41 @@
+/**
+ * This script takes a CSV file and its data schema and uploads it to the Amazon AWS S3 database.
+ * These resources are to be used to create an AWS machine learning model, using aws-ml-file-uploader.js
+ *
+ * NOTE: To successfully use this script, you must include a valid CSV file, a valid schema
+ * (valid as in the same format that Amazon uses), and a policy.json that permits Amazon to
+ * read from the S3 buckets. These  files MUST be in a directory titled requiredFiles, also in the working
+ * directory of the program.
+ * The policy included in the requiredDocuments folder can be modified by changing the Bucket name and the CSV file name.
+ *
+ * @summary   Takes a CSV file and its data schema, uploads it to the Amazon AWS S3 database.
+ * @author Andrew Smith
+ * 6-12-2017
+ *
+ */
+
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 AWS.config.update({region:'us-east-1'});
+
 var colors = require('colors');
 var async = require('async');
 var path = require('path');
 var glob = require('glob');
-var fs = require('fs');
 var globOptions = {dot:true};
+var fs = require('fs');
+
+/**
+ * @summary This function both creates the bucket needed to store the files, and populates it with the required data.
+ *
+ * @param String $bucketName User provided name for the bucket.
+ * @param function $done Callback for the function.
+ * @return no return.
+ */
 
 var createBucket = function(bucketName, done) {
     async.series([
-        function(callback){
+        function(callback){ //Creates the bucket, and populates it with the a policy file.
             s3.createBucket({Bucket: bucketName}, function (err, data) {
                 console.log("Creating bucket");
                 if (err) console.log("Error:" + err);
@@ -30,7 +55,7 @@ var createBucket = function(bucketName, done) {
                 });
             });
         },
-        function(callback){
+        function(callback){ //Finds the required CSV file and uploads it
             glob(path.join('requiredFiles/*.csv*'), globOptions, function(err, matches) {
                 var filePath = matches[0];
                 var csvFile = filePath.substring(14);
