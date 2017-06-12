ea9d625f303751b109f04e5653cf97f5f536fbf5
Andrew Smith <andy-smith@comcast.net>
2017-06-12 10:24:04 -0400

    First working

--- aws-ml-file-uploader.js
+++ aws-ml-file-uploader.js
@@ -7,38 +7,64 @@ var bucketName = process.argv[2];
 var uploadedFile = process.argv[3];
 var dataSchema = process.argv[4];
 var async = require('async');
+var glob = require('glob');
+var path = require('path');
+var globOptions = {dot:true};
 // Bucket names must be unique across all S3 users
+var bucketName = "testAS1043";
+var dataParams;
+var mlParams;
+var csv;
+var schema;
 
-var dataParams = {
-  DataSourceId: uploadedFile,
-  DataSpec: {
-      DataLocationS3: 's3://' + bucketName + '/' + uploadedFile,
-      DataSchemaLocationS3: 's3://' + bucketName + '/' + uploadedFile + ".schema"
-  },
-  ComputeStatistics: true,
-  DataSourceName: uploadedFile
-};
-var mlParams = {
-    MLModelId: 'test-model', /* required */
-    MLModelType: "BINARY", /* required */
-    TrainingDataSourceId: uploadedFile, /* required */
-    MLModelName: 'Test Model'
-};
-var waiterParams = {
-    FilterVariable: "Name",
-    EQ: uploadedFile
-};
+
+function createParameters(done) {
+    glob(path.join('requiredFiles/*.csv*'), globOptions, function(err, matches) {
+        csv = matches[0].substring(14);
+        schema = matches[1].substring(14);
+        dataParams = {
+            DataSourceId: csv,
+            DataSpec: {
+                DataLocationS3: 's3://' + bucketName + '/' + csv,
+                DataSchemaLocationS3: 's3://' + bucketName + '/' + csv + ".schema"
+            },
+            ComputeStatistics: true,
+            DataSourceName: csv
+        };
+        mlParams = {
+            MLModelId: csv + "-model", /* required */
+            MLModelType: "BINARY", /* required */
+            TrainingDataSourceId: csv, /* required */
+            MLModelName: csv + "-model"
+        };
+        done();
+    });
+}
 async.series([
+    function(callback) {
+        createParameters(function() {
+            console.log("Parameters for bucket created.")
+            callback();
+        });
+    },
     function(callback){
-        createBucket(bucketName, uploadedFile, dataSchema, function(err, data) {
-            console.log('Bucket Created.');
-            callback(err);
-            });
+        createBucket(bucketName, function(err) {
+            console.log("Created Bucket");
+            callback();
+        });
     },
     function(callback){
         machinelearning.createDataSourceFromS3(dataParams, function(err, data) {
+            if (err) console.log("ERR" + err, err.stack); // an error occurred
             console.log('Data Source Created for ML');
+            callback();
+        });
+    },
+    function(callback){
+        machinelearning.createMLModel(mlParams, function(err, data) {
             if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+            console.log("Model Created");
             callback(err);
         });
     }
--- createBucket.js
+++ createBucket.js
@@ -8,66 +8,66 @@ var glob = require('glob');
 var fs = require('fs');
 var globOptions = {dot:true};
 
-var bucketName = "testAS1043";
-
 var createBucket = function(bucketName, done) {
     async.series([
         function(callback){
             s3.createBucket({Bucket: bucketName}, function (err, data) {
                 console.log("Creating bucket");
                 if (err) console.log("Error:" + err);
-                callback(err);
+                glob(path.join('requiredFiles/*.JSON*'), globOptions, function(err, matches) {
+                    var params;
+                    fs.readFile(matches[0],'utf8', function(err, fileBuffer) {
+                        params = {
+                            Bucket: bucketName, /* required */
+                            Policy: fileBuffer /* required */
+                        };
+                        s3.putBucketPolicy(params, function(err, data) {
+                            if (err) console.log(err, err.stack); // an error occurred
+                            else     console.log(data);           // successful response
+                            callback(err);
+                        });
+                    });
+                });
             });
         },
         function(callback){
-            glob(path.join('*.json'), globOptions, function(err, matches) {
-
-
-                fs.readFile(path.join(matches[0]), 'utf8', function (err, fileBuffer) {
-                    console.log('Reading Policy File...'.bold);
+            glob(path.join('requiredFiles/*.csv*'), globOptions, function(err, matches) {
+                var filePath = matches[0];
+                var csvFile = filePath.substring(14);
+                fs.readFile(filePath, 'utf8', function (err, fileBuffer) {
+                    console.log('Reading CSV File...'.bold);
                     if (err) return console.log("Error:" + err);
 
-                    var params = {
-                        Bucket: bucketName,
-                        Policy: fileBuffer
-                    };
-                    s3.putBucketPolicy(params, function(err, data) {
-                        if (err) console.log(err, err.stack); // an error occurred
-                        else     console.log(data);           // successful response
+                    params = {Bucket: bucketName, Key: csvFile, Body: fileBuffer};
+                    s3.putObject(params, function (err, data) {
+                        if (err) {
+                            console.log("Error:" + err)
+                        } else {
+                            console.log("Successfully uploaded data to ".green + bucketName.bold + "/".bold + csvFile.bold);
+                        }
                         callback(err);
                     });
                 });
             });
         },
         function(callback){
-            glob(path.join('*.csv*'), globOptions, function(err, matches) {
-                fs.readFile(matches[0], 'utf8', function (err, fileBuffer) {
-                    console.log('Reading CSV File...'.bold);
-                    if (err) return console.log("Error:" + err);
-
-                    params = {Bucket: bucketName, Key: matches[0], Body: fileBuffer};
-                    s3.putObject(params, function (err, data) {
-                        if (err) {
-                            console.log("Error:" + err)
-                        } else {
-                            console.log("Successfully uploaded data to ".green + bucketName.bold + "/".bold + matches[0].bold);
-                        }
-                    });
-                });
-                fs.readFile(matches[1], 'utf8', function (err, fileBuffer) {
+            glob(path.join('requiredFiles/*.csv*'), globOptions, function(err, matches) {
+                var filePath = matches[1];
+                var dataSchema = filePath.substring(14);
+                fs.readFile(filePath, 'utf8', function (err, fileBuffer) {
                     console.log('Reading Schema File...'.bold);
                     if (err) return console.log("Error:" + err);
 
-                    params = {Bucket: bucketName, Key: matches[1], Body: fileBuffer};
+                    params = {Bucket: bucketName, Key: dataSchema, Body: fileBuffer};
                     s3.putObject(params, function (err, data) {
                         if (err) {
                             console.log("Error:" + err)
                         } else {
-                            console.log("Successfully uploaded data to ".green + bucketName.bold + "/".bold + matches[1].bold);
+                            console.log("Successfully uploaded data to ".green + bucketName.bold + "/".bold + dataSchema.bold);
                         }
+                        callback(err);
                     });
                 });
-                callback(err);
             });
         }
     ], function (err, data) {
@@ -75,11 +75,6 @@ var createBucket = function(bucketName, done) {
         else console.log(data);
         done();
     });
-
 };
 
 module.exports = createBucket;
-
-createBucket(bucketName, function() {
-    console.log("Done")
-});
